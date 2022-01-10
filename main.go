fc3f20a2625013f1969bab9e944e553d6afd4daf
Akhmad Maulana Akbar <akhmad0598@gmail.com>
2022-01-11 02:54:22 +0700

    change to struct, utilize channel to get data from different page and element; will add Auto-Connect functionality later

--- main.go
+++ main.go
@@ -3,58 +3,121 @@ package main
 import (
 	"errors"
 	"log"
+	"os"
+	"time"
 
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/proto"
+	"github.com/joho/godotenv"
 )
 
-const admin_url = "http://192.168.1.1"
-const username = "admin"
-const password = "Telkomdso123"
-
-func main() {
-	launcher := launcher.New().Headless(false)
-	defer launcher.Cleanup()
-
-	url := launcher.MustLaunch()
-	browser := rod.New().ControlURL(url).Trace(true).MustConnect()
-	defer browser.MustClose()
-
-	page := browser.MustPage(admin_url)
-
-	err := login(page)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	iframe_template := page.MustElement("iframe[src='template.gch']").MustFrame()
-	IPAddress, err := getIPAddress(iframe_template)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println(IPAddress)
+type Page struct {
+	page      *rod.Page
+	admin_url string
+	username  string
+	password  string
 }
 
-func login(page *rod.Page) error {
-	page.MustElement("input[id='Frm_Username']").MustInput(username)
-	page.MustElement("input[id='Frm_Password']").MustInput(password)
-	page.MustElement("input[id='LoginId']").MustClick()
+func NewPage(page *rod.Page, admin_url, username, password string) *Page {
+	return &Page{
+		page:      page,
+		admin_url: admin_url,
+		username:  username,
+		password:  password,
+	}
+}
 
-	if exist, errmsg, err := page.Has("font[id='errmsg']"); exist || err != nil {
+func (p *Page) Login() error {
+	time.Sleep(200 * time.Millisecond)
+	p.page.MustElement("input[id='Frm_Username']").MustInput(p.username)
+	p.page.MustElement("input[id='Frm_Password']").MustInput(p.password)
+	p.page.MustElement("input[id='LoginId']").MustClick()
+
+	if exist, errmsg, err := p.page.Has("font[id='errmsg']"); exist || err != nil {
 		return errors.New(errmsg.String())
 	}
 
 	return nil
 }
 
-func getIPAddress(page *rod.Page) (string, error) {
-	// logout := page.MustElementR("a", "Logout")
-	// // defer logout.MustClick()
-
-	network_info := page.MustElement("table.menu_table tr.h2_content")
+func (p *Page) WANInfoPage() *rod.Page {
+	network_info := p.page.MustElement("table.menu_table tr.h2_content")
 	network_info.MustClick()
 
-	IPAddress := page.MustElement("input[id='TextPPPIPAddress0']").MustAttribute("value")
+	return network_info.Page()
+}
+
+func (p *Page) GetIPAddress(ipAddress chan string) (string, error) {
+	IPAddress := p.page.MustElement("input[id='TextPPPIPAddress0']").MustAttribute("value")
+
+	ipAddress <- *IPAddress
 	return *IPAddress, nil
+}
+
+func (p *Page) GetConnStatus(connStatus chan string) (string, error) {
+	ConnStatus := p.page.MustElement("input[id='TextPPPConStatus0']").MustAttribute("value")
+
+	connStatus <- *ConnStatus
+	return *ConnStatus, nil
+}
+
+func (p *Page) Logout(signal chan bool) {
+	if err := p.page.MustElement("div[class='title_log'] > a").Click(proto.InputMouseButtonLeft); err != nil {
+		log.Println(err)
+	}
+
+	signal <- true
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panicln("There is no .env found in the directory, please add!")
+	}
+
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	admin_url := os.Getenv("ADMIN_URL")
+
+	launcher := launcher.New().Headless(true)
+	defer launcher.Cleanup()
+
+	url := launcher.MustLaunch()
+	browser := rod.New().ControlURL(url).Trace(true).MustConnect()
+	defer browser.MustClose()
+
+	page := NewPage(browser.MustPage(admin_url), admin_url, username, password)
+
+	err = page.Login()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bodyPage := NewPage(page.page.MustElement("iframe[src='template.gch']").MustFrame(), admin_url, username, password)
+
+	connInfoPage := NewPage(bodyPage.WANInfoPage(), admin_url, username, password)
+
+	IPAddress := make(chan string, 1)
+	ConnStatus := make(chan string, 1)
+	go func() {
+		_, err = connInfoPage.GetIPAddress(IPAddress)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		_, err = connInfoPage.GetConnStatus(ConnStatus)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+	}()
+
+	log.Println("IP Address: ", <-IPAddress)
+	log.Println("Status: ", <-ConnStatus)
+
+	signalSuccessLogout := make(chan bool, 1)
+
+	go bodyPage.Logout(signalSuccessLogout)
+
+	log.Println("Logout? ", <-signalSuccessLogout)
 }
